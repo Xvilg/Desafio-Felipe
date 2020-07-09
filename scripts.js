b08a037da3e7f1338d5036cf7f5e2c80f4be646d
Xvilg <60821055+Xvilg@users.noreply.github.com>
2020-07-08 22:49:51 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,25 @@
 function GetDados() {
     fetch("https://reqres.in/api/users").then(res => res.json()).then(dados => {
         const Dado = dados.data;
+        
         for (i = 0; i < Dado.length; i++) {
-
-
             const GetTbody = document.querySelector("tbody");
             const createTr = document.createElement("tr");
             const createTh = document.createElement("th");
-
             const text = document.createTextNode(i + 1)
             createTh.setAttribute("scope", "row")
             createTh.appendChild(text)
             createTr.appendChild(createTh)
             GetTbody.appendChild(createTr)
-
             createTh.appendChild(text)
-
-
+            
             for (D = 1; D < 6; D++) {
                 const createTd = document.createElement("td")
                 const TextTd = document.createTextNode("")
                 createTd.appendChild(TextTd)
                 createTr.appendChild(createTd)
                 const Dt = dados.data[i];
-
-
+                
                 if (D == 1) {
                     createTd.textContent = Dt.id;
                 } else if (D == 2) {
@@ -36,40 +31,28 @@ function GetDados() {
                 } else {
 
                     const link = document.createElement("a")
-                    
-
+                   
                     const buro = document.createElement("i");
                     buro.setAttribute("class", "fab fa-buromobelexperte")    
-                    
-                    
-
+                                   
                     if (Dt.first_name.length < 5) {
                         const appleAlt = document.createElement("i");
                         appleAlt.setAttribute("class", "fas fa-apple-alt")
-
-
                         createTd.appendChild(appleAlt)   
 
                     } else {
                         link.setAttribute("href", Dt.avatar) 
                         const apple = document.createElement("i");
                         apple.setAttribute("class", "fab fa-apple");
-                        
-                        
-                                                                        
+                                                                                                                      
                         createTd.appendChild(apple)                                                                                                     
                     }
                     
                     link.appendChild(buro)
                     createTd.appendChild(link)
                 }
-
-            }
-            
+            }          
         }
-
     })
-
-
 }
 GetDados()
